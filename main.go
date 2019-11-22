ed1cea6860044a02f04c1f235adbff36d406fb11
Manigandan Dharmalingam <manigandan.jeff@gmail.com>
2019-11-22 16:18:19 +0530

    read influxdb using flux query string

--- main.go
+++ main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"context"
-	"encoding/csv"
+	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
-	"strings"
 	"time"
 
 	"github.com/influxdata/influxdb-client-go"
@@ -49,19 +47,19 @@ type IncidentMet struct {
 
 // IncidentMetric ...
 type IncidentMetric struct {
-	Result string `json:"result" flux:"result"`
+	Result string `json:"result,omitempty" flux:"result"`
 	Table  string `json:"table" flux:"table"`
 	// _time
-	Start time.Time `json:"_start"  flux:"_start"`
-	Stop  time.Time `json:"_stop" flux:"_stop"`
-	Time  time.Time `json:"_time" flux:"_time"`
+	Start time.Time `json:"start"  flux:"_start"`
+	Stop  time.Time `json:"stop" flux:"_stop"`
+	Time  time.Time `json:"time" flux:"_time"`
 
 	// _fields
-	Value interface{} `json:"_value" flux:"_value"`
-	Field string      `json:"_field" flux:"_field"`
+	Value interface{} `json:"value" flux:"_value"`
+	Field string      `json:"field" flux:"_field"`
 
 	// _measurement
-	Measurement string `json:"_measurement" flux:"_measurement"`
+	Measurement string `json:"measurement" flux:"_measurement"`
 
 	// _tags
 	IncidentID    string `json:"incident_id" flux:"incident_id"`
@@ -75,8 +73,7 @@ type IncidentMetric struct {
 // map[ alert_source_id:8 event_type:9 incident_id:10 orgnization_id:11  service_id:12 status:13 ]
 
 const (
-	influxOrgName = "GopherHut"
-	// bucketName          = "incident"
+	influxOrgName       = "GopherHut"
 	bucketName          = "test_bucket"
 	myHTTPInfluxAddress = "http://localhost:9999"
 	myToken             = "RM2Ezx1ZYedbIqjs-_PHBGUN3MVB1RzIan_qwxZf7BTlyIT12RBqJVuPJIgMX91DPB9-YjB3T-rPvVxfrpUAdA=="
@@ -100,8 +97,12 @@ func main() {
 	|> filter(fn: (r) => r.alert_source_id == "5dd524fc8c61caf1309984ce")
 	|> filter(fn: (r) => r.event_type == "acknowledged")`
 
-	var extern IncidentMet
-	res, err := influx.QueryCSV(bg(), fluxQuery, influxOrgName, extern)
+	// var extern IncidentMet
+	// extern := map[string]interface{}{
+	// 	"result":          111,
+	// 	"alert_source_id": "manigandan",
+	// }
+	res, err := influx.QueryCSV(bg(), fluxQuery, influxOrgName)
 	if err != nil {
 		log.Fatal("Read Err: ", err)
 	}
@@ -109,28 +110,20 @@ func main() {
 		log.Fatal("Read Err: ", res.Err)
 	}
 
-	type QueryCSVResult struct {
-		io.ReadCloser
-		csvReader   *csv.Reader
-		Row         []string
-		ColNames    []string
-		colNamesMap map[string]int
-		dataTypes   []string
-		group       []bool
-		defaultVals []string
-		Err         error
-	}
-
+	var incidentMetrics []*IncidentMetric
 	for res.Next() {
-		fmt.Println("Row: ", res.Row)
-		fmt.Println("ColNames: ", res.ColNames)
-
+		// fmt.Println("Row: ", res.Row)
+		// fmt.Println("ColNames: ", res.ColNames)
 		var data IncidentMetric
 		if err := res.Unmarshal(&data); err != nil {
 			log.Fatal("Unmarshal Err: ", err)
 		}
-		fmt.Printf("%+v\n", data)
-		break
+		// fmt.Printf("%+v\n", data)
+		incidentMetrics = append(incidentMetrics, &data)
+	}
+
+	if bytes, err := json.Marshal(incidentMetrics); err == nil {
+		fmt.Printf("%+v\n", string(bytes))
 	}
 
 	if err := influx.Close(); err != nil {
@@ -139,7 +132,6 @@ func main() {
 }
 
 func writeData(client *influxdb.Client) {
-	primitive.NewObjectID()
 	measurement := "incident-metrics"
 	organizationID := primitive.NewObjectID().Hex()
 	serviceID := primitive.NewObjectID().Hex()
@@ -147,8 +139,6 @@ func writeData(client *influxdb.Client) {
 
 	var allIncidentMerics []influxdb.Metric
 	now := time.Now()
-	fmt.Println(now)
-	fmt.Println(strings.Repeat("-", 80))
 
 	for i := 10; i >= 1; i-- {
 		incidentID := primitive.NewObjectID().Hex()
@@ -158,12 +148,12 @@ func writeData(client *influxdb.Client) {
 		resolvedAt := triggeredAt.Add((time.Duration(i) * time.Minute))
 		tta := acknowledgedAt.Sub(triggeredAt).Seconds()
 		ttr := resolvedAt.Sub(triggeredAt).Seconds()
-		fmt.Println("triggeredAt: ", triggeredAt)
-		fmt.Println("acknowledgedAt: ", acknowledgedAt)
-		fmt.Println("resolvedAt: ", resolvedAt)
-		fmt.Println("TTA: ", tta)
-		fmt.Println("TTR: ", ttr)
-		fmt.Println(strings.Repeat("~", 80))
+		// fmt.Println("triggeredAt: ", triggeredAt)
+		// fmt.Println("acknowledgedAt: ", acknowledgedAt)
+		// fmt.Println("resolvedAt: ", resolvedAt)
+		// fmt.Println("TTA: ", tta)
+		// fmt.Println("TTR: ", ttr)
+		// fmt.Println(strings.Repeat("~", 80))
 
 		incidentMerics := []influxdb.Metric{
 			influxdb.NewRowMetric(
